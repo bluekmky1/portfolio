673a1af58b6545cf343366db2a3d5d689b3dbe7e
bluekmky1 <bluekmky@gmail.com>
2025-10-09 15:07:00 +0900

    🔧 Enable path URL strategy for improved routing in web application

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'routes/app_router.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(const ProviderScope(child: MainApp()));
 }
 
