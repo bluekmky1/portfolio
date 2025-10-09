766fe0edd6933a35ba032d92422d7c59d6e04555
bluekmky1 <bluekmky@gmail.com>
2025-10-09 14:30:14 +0900

    🔧 Remove custom domain and Jekyll settings from web deployment workflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'routes/app_router.dart';
 
 void main() {
-  usePathUrlStrategy();
   runApp(const ProviderScope(child: MainApp()));
 }
 
